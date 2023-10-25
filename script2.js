d8d36f59cd3dec97bb41e41d9ce6e47e173ed71b
joshtype <115691670+joshtype@users.noreply.github.com>
2023-10-25 03:27:19 +0000

    Adding to branch 2. Trying to resolve the as yet unresolved js bug.

---
+++ script2.js
@@ -0,0 +1,8 @@
+
+  
+const usernames = ["Admin","User","Henry","Elizabeth","Shana"];
+
+
+
+const form = document.getElementById("p3-form");
+form.addEventListener("submit", validateUserPass, true);
