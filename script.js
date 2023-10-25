222568b3acdf2cf7041a9a459d6a4f6f95e0db78
joshtype <115691670+joshtype@users.noreply.github.com>
2023-10-25 08:17:58 +0000

    Resolved JS bug and replit fork. Updated CSS.

--- script.js
+++ script.js
@@ -5,44 +5,46 @@
 // Project Site JS File //
 //======================//
 
-// Function to validate username/password inputs
-const validateUserPass = function(event) {
-  // prevent submission while validating
-  event.preventDefault();
-  
-  // arr of registered usernames
-  let userDB = ["Admin", "User", "Henry", "Elizabeth", "Shana", "TigerKing"];
-  
-  // VALIDATE USERNAME INPUT
-  let userInput = document.getElementById("user");
-  // traverse arr
-  for(let i = 0; i < userDB.length; i++) {
-    // check ith idx
-    if(userDB[i] === userInput) {
-      alert("Username not available.");  
-    } else {
-      continue;  // check i+1 idx
-    }
-  }
-
-  // VALIDATE PASSWORD INPUTS
-  let pass1 = document.getElementById("pass1");
-  let pass2 = document.getElementById("pass2");
-  // check password len
-  if(pass1.length < 8) {
-    // alert user
-    alert("Password must be at least 8 characters.")
-  }
-  // check inputs match
-  if(pass1.value !== pass2.value) {
-    // RETURNS THIS ALERT NO MATTER WHAT
-    alert("Passwords do not match.")
-  }
+// function to remove error msg when retrying invalid user/pass
+const clearErr = function() {
+  document.getElementById("user-err").innerHTML = "";
+  document.getElementById("pass1-err").innerHTML = "";
+  document.getElementById("pass2-err").innerHTML = "";
 };
 
-// THIS SHOULDNT BE NECESSARY WITH onsubmit PROPERTY ADDED
-const form = document.getElementById("reg-form");
-form.addEventListener("submit", validateUserPass, true);
-// RETURNS TYPEERROR: cannot read properties of null
-// BUT WITHOUT IT, IT DOESN'T VALIDATE
+//function to validate username and password inputs
+const formValidate = function() {
+  // search dom for input using name attributes & store the value
+  let userInp = document.forms["p3-form"]["user"].value;
+  // arr of unavailable usernames
+  let usernames = ["Admin","User","Henry","Elizabeth","Shana"];
+  // traverse over arr of unavailable usernames
+  for(let i = 0; i < usernames.length; i++) {
+    // compare input to ea ith idx
+    if(usernames[i] === userInp) {
+      // display error msg under input field
+      let text = document.getElementById("user-err");
+      let errMsg = "Username not available.";
+      text.innerHTML = errMsg;
+      return false;  // prevent form submission
+    } else {
+      continue;  // check next idx
+    }
+  }
+  // search dom for field using name attribute, store value
+  let pass1 = document.forms["p3-form"]["pass1"].value;
+  let pass2 = document.forms["p3-form"]["pass2"].value;
 
+  // validate password input len
+  if(pass1.length < 8) {
+    let errMsg = "Password must be at least 8 characters.";
+    document.getElementById("pass1-err").innerHTML = errMsg;
+    return false;
+  }
+  // validate str equality of inputs
+  if(pass1 !== pass2) {
+    let errMsg = "Confirmation must match password.";
+    document.getElementById("pass2-err").innerHTML = errMsg;
+    return false;
+  }
+};
--- script2.js
+++ script2.js
@@ -1,8 +1,36 @@
-
+//======================//
+// Joshua Gregory       //
+// Professor Montgomery //
+// Internet Programming //
+// Project Site JS File //
+//======================//
   
-const usernames = ["Admin","User","Henry","Elizabeth","Shana"];
 
+const formValidate = function() {
 
+  // VALIDATE USERNAME INPUT
+  let usernames = ["Admin","User","Henry","Elizabeth","Shana"];
+  let userInp = document.forms["p3-form"]["user"].value;
+  for(let i = 0; i < usernames.length; i++) {
+    if(usernames[i] === userInp) {
+      alert("Username not available.");
+      return false;
+    } else {
+      continue;
+    }
+  }
 
-const form = document.getElementById("p3-form");
-form.addEventListener("submit", validateUserPass, true);
+  // VALIDATE PASSWORD INPUTS
+  let pass1 = document.forms["p3-form"]["pass1"].value;
+  let pass2 = document.forms["p3-form"]["pass2"].value;
+
+  if(pass1.length < 8) {
+    alert("Password must be at least 8 characters.");
+    return false;
+  }
+    
+  if(pass1 !== pass2) {
+    alert("Passwords do not match.");
+    return false;
+  }
+};
